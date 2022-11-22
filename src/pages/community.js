9f79811dd74de40db65bcb03904fdf27f9f838be
조용제 <whehdrms6810@naver.com>
2022-11-22 15:47:12 +0900

    main페이지 레이아웃 추가

---
+++ src/pages/community.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+export const Community = () => {
+  // const [category, setCategory] = useState();
+  return (
+    <Frame>
+      <Aside>
+        <h4>공지사항</h4>
+        <hr />
+        <span>홈페이지 이용 안내</span>
+        <span>홈페이지 이용 안내</span>
+        <span>홈페이지 이용 안내</span>
+      </Aside>
+      <div>안에 내용</div>
+    </Frame>
+  );
+};
+
+const Frame = styled.div`
+  display: flex;
+  margin-top: 15px;
+  column-gap: 10px;
+  width: 90%;
+  height: 100%;
+`;
+
+const Aside = styled.aside`
+  display: flex;
+  flex-direction: column;
+  width: 17%;
+  height: 50vh;
+  background-color: #efefef;
+`;
---
+++ src/style/news.style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+export const NewsBox = styled.article`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+
+export const Line = styled.hr`
+  width: 100%;
+  height: 3px;
+  background-color: #ededed;
+  border: none;
+  margin: 20px 0;
+`;
+
+export const ItemBox = styled.div`
+  width: 30%;
+`;
+
+export const NewsItem = styled.img`
+  width: 30%;
+`;
