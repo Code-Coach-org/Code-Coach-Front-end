f29ece1c93083ee723a7927e7db0327d084a3234
youngmin <min050410@gmail.com>
2022-11-29 22:44:03 +0900

    add: 수업 출석 페이지

--- src/pages/class/classAttendance/ClassAttendancePage.jsx
+++ src/pages/class/classAttendance/ClassAttendancePage.jsx
@@ -1,41 +1,77 @@
 import React from "react";
+import { useState } from "react";
+import ReactCodeInput from "react-code-input";
 import ClassHeaderComponent from "../../../components/class/ClassHeader";
-import * as S from "../../../styles/class/classAttendance/classAttendance.style";
+import * as S from "../../../styles/class/classAttendance/classAttendance.style"; 
 
 const ClassAttendancePage = () => {
 
-    const EXAMPLE_PDF_URL = "https://github.com/Code-Coach-org/Code-Coach-Front-end/files/10072275/1.pdf";
+    // TODO :: 남은 횟수 서버 저장 
+    const [isCodeValid, setCodeValid] = useState(10);
+    const [code, setCode] = useState("");
+    const [btnIsPressed, setBtnIsPressed] = useState(false);    
+
+    const CORRECT_PIN_CODE = "111111";
+
+    const checkCode = () => {
+        const isPinCodeValid = code === CORRECT_PIN_CODE;
+    
+        setBtnIsPressed(true);
+        setCodeValid(prev => prev-1);
+        if (!isPinCodeValid) setCode("");
+    };
+
+    const handleCodeChange = Code => {
+        setCode(Code);
+        setBtnIsPressed(false);
+      };
 
     return (
         <S.Layout>
             <ClassHeaderComponent />
-            <>
-                <>
-                    <></>
-                    <></>
-                </>
-                <></>
-                <>
-                    <></>
-                    <></>
-                </>
-            </>
-            <>
-                <>
-                    <>날짜</>
-                    <>출석</>
-                    <>결석</>
-                    <>지각</>
-                    <>조회</>
-                </>
-                <>
-                    <>합계: 20</>
-                    <>출석: 11</>
-                    <>결석: 9</>
-                    <>지각: 0</>
-                    <>조회: 0</>
-                </>
-            </>
+            <S.Warpper>
+                <S.Date>
+                    <S.Today>2022-11-15</S.Today>
+                    <S.Time>03:20</S.Time>
+                </S.Date>
+                <ReactCodeInput
+                    fields={4}
+                    onChange={handleCodeChange}
+                    value={code}
+                />
+                <S.ButtonWarpper>
+                    <S.Remaining>남은 횟수: 7</S.Remaining>
+                    <S.Button>
+                        출석 
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                        </svg>
+                    </S.Button>
+                </S.ButtonWarpper>
+            </S.Warpper>
+            <S.Schedule>
+                <S.ScheduleRow>
+                    <S.Item>날짜</S.Item>
+                    <S.Item>출석</S.Item>
+                    <S.Item>결석</S.Item>
+                    <S.Item>지각</S.Item>
+                    <S.Item>조회</S.Item>
+                </S.ScheduleRow>
+                <S.ScheduleRow>
+                    <S.Item>2022-11-11</S.Item>
+                    <S.Item>O</S.Item>
+                    <S.Item></S.Item>
+                    <S.Item></S.Item>
+                    <S.Item></S.Item>
+                </S.ScheduleRow>
+            </S.Schedule>
+            <S.Result>
+                <S.ResultItem>합계: 20</S.ResultItem>
+                <S.ResultItem>출석: 11</S.ResultItem>
+                <S.ResultItem>결석: 9</S.ResultItem>
+                <S.ResultItem>지각: 0</S.ResultItem>
+                <S.ResultItem>조회: 0</S.ResultItem>
+            </S.Result>
         </S.Layout>
     );
 };
--- src/styles/class/classAttendance/classAttendance.style.js
+++ src/styles/class/classAttendance/classAttendance.style.js
@@ -1,8 +1,85 @@
 import styled from "styled-components";
+import { ANCHOR_ACTIVE_COLOR } from "../../constants/color.constant";
 
 export const Layout = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
   flex-direction: column;
+`
+
+export const Warpper = styled.div`
+  display: flex;
+  width: 95%;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Date = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Today = styled.div`
+  font-size: 1.5em;
+`
+
+export const Time = styled.div`
+  font-size: 3rem;
+  font-weight: 600;
+`
+
+export const ButtonWarpper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Remaining = styled.div`
+  
+`
+
+export const Button = styled.button`
+  display: flex;
+  height: 2rem;
+  width: 4rem;
+  background-color: #77AEEF;
+  border: none;
+  color: white;
+  align-items: center;
+  justify-content: center;
+  > svg {
+    width: 1rem;
+  }
+`
+ 
+export const Schedule = styled.div`
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid black;
+`
+
+export const ScheduleRow = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 10fr 1fr 1fr 1fr 1fr;
+  justify-items: center;
+  padding: .5rem 0;
+  border-top: 1px solid black;
+  /* align-items: center; */
+`
+
+export const Item = styled.div`
+  text-align: center;
+  width: 100%;
+`
+export const Result = styled.div`
+  width: 95%;
+  display: flex;
+  border-bottom: 1px solid black;
+  padding: 1rem 0rem;
+`
+
+export const ResultItem = styled.div`
+  margin: 0rem 2rem;
 `
