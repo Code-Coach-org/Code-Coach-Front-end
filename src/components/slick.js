4ba68cff5492c7a40d85f8fba2490ff36a0f8eeb
조용제 <whehdrms6810@naver.com>
2022-11-29 14:39:43 +0900

    [UPDATE] carousel slider

--- src/components/slick.js
+++ src/components/slick.js
@@ -1,48 +1,73 @@
-import { Carousel } from "react-carousel-minimal";
+// import { Carousel } from "react-carousel-minimal";
 
 import React from "react";
 import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 export default function Slick() {
-  const data = [
-    {
-      image: "",
-    },
-    {
-      image: "",
-    },
-    {
-      image: "",
-    },
-  ];
-  const slideNumberStyle = {
-    fontSize: "20px",
-    fontWeight: "bold",
+  const settings = {
+    dots: false, // 점은 안 보이게
+    infinite: true, // 무한으로 즐기게
+    speed: 500,
+    slidesToShow: 1, //4장씩 보이게 해주세요
+    slidesToScroll: 1, //1장씩 넘어가세요
+    dots: true,
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
   return (
-    <SlickBox>
-      <Carousel
-        data={data}
-        time={5000}
-        height="200px"
-        width="100%"
-        radius="10px"
-        marginTop="10px"
-        slideNumber={true}
-        slideNumberStyle={slideNumberStyle}
-        captionPosition="bottom"
-        automatic={true}
-        dots={true}
-        pauseIconColor="white"
-        pauseIconSize="40px"
-        slideBackgroundColor="darkgrey"
-        slideImageFit="cover"
-        thumbnailWidth="100px"
-      />
-    </SlickBox>
+    <Wrap>
+      <div>
+        <Slider {...settings}>
+          <div>
+            <h2>1</h2>
+            {/* <img src="" alt=""></img> */}
+          </div>
+          <div>
+            <h2>2</h2>
+            {/* <img src="" alt=""></img> */}
+          </div>
+          <div>
+            <h2>3</h2>
+            {/* <img src="" alt=""></img> */}
+          </div>
+        </Slider>
+      </div>
+    </Wrap>
   );
 }
 
-const SlickBox = styled.div`
-  margin-top: 20px;
+const Wrap = styled.div`
+  margin: 5% auto;
+  width: 100%;
+  .slick-prev:before {
+    opacity: 1; // 기존에 숨어있던 화살표 버튼이 보이게
+    color: black; // 버튼 색은 검은색으로
+    left: 0;
+  }
+  .slick-next:before {
+    opacity: 1;
+    color: black;
+  }
 `;
